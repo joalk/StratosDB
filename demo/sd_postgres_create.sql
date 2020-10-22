4c48aef071a213d2df961f447702dd555f476070
joalk <joalkims@gmail.com>
2020-10-22 16:43:34 -0700

    added to demo database
    Co-authored-by: Sophia Pak <sophiakpak@gmail.com>
    Co-authored-by: Tommy Song <tommysong123@gmail.com>
    Co-authored-by: Tyler Morgan <tylermor562@gmail.com>

--- demo/sd_postgres_create.sql
+++ demo/sd_postgres_create.sql
@@ -58,12 +58,19 @@ INSERT INTO public.owners VALUES (2, 'jk@gmail.com', 'Joal', 'Kim', 'helloworld4
 INSERT INTO public.owners VALUES (3, 'ts@gmail.com', 'Tommy', 'Song', 'helloworld789', 3);
 INSERT INTO public.owners VALUES (4, 'tm@gmail.com', 'Tyler', 'Morgan', 'helloworld0-=', 4);
 INSERT INTO public.owners VALUES (5, 'ns@gmail.com', 'Nicholas', 'Shay', 'helloworld321', 5);
+INSERT INTO public.owners VALUES (6, 'mh@gmail.com', 'Michael', 'OHalloran', 'helloworld321', 6);
+INSERT INTO public.owners VALUES (7, 'mm@gmail.com', 'Miguel', 'Michel', 'helloworld321', 7);
 
 INSERT INTO public.dogs VALUES (1, 'Pancake', 'Pembroke Welsh Corgi', 6, 1);
 INSERT INTO public.dogs VALUES (2, 'Honey', 'Pembroke Welsh Corgi', 1, 1);
 INSERT INTO public.dogs VALUES (3, 'Oreo', 'Dalmatian', 17, 2);
 INSERT INTO public.dogs VALUES (4, 'Rex', 'Belgian Malinois', 5, 1);
-INSERT INTO public.dogs VALUES (5, 'Duke', 'Pembroke Welsh Corgi', 7, 5);
+INSERT INTO public.dogs VALUES (5, 'Cookie', 'German Shepherd', 1, 2);
+INSERT INTO public.dogs VALUES (6, 'Fluffy', 'Maltese Bichon Frise', 2, 2);
+INSERT INTO public.dogs VALUES (7, 'Duke', 'Pembroke Welsh Corgi', 7, 5);
+INSERT INTO public.dogs VALUES (8, 'Lord Regis', 'Chihuahua Yorkshire Terrier', 7, 6);
+INSERT INTO public.dogs VALUES (9, 'Ramses the Great', 'Chihuahua Yorkshire Minpin Brittany', 5, 6);
+INSERT INTO public.dogs VALUES (10, 'Rico', 'Pitbull Labrador', 4, 7);
 
 INSERT INTO public.cats VALUES (1, 'Mr. Flufflers', 'Ragdoll', 1, 3);
 INSERT INTO public.cats VALUES (2, 'Mr. Pickles', 'British Short Hair', 2, 5);
